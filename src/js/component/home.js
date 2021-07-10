dc3bcae865024769ebd02df2f8f1cba9af418b1a
Jonathan Diaz <64094787+Jonathan-0708@users.noreply.github.com>
2021-07-10 18:21:35 -0400

    cambios

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 export function Home() {
+	const [selected, setSelected] = useState("");
+
+	const changeColor = color => {
+		if (color == selected) {
+			setSelected("");
+		} else {
+			setSelected(color);
+		}
+	};
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="trafficTop"></div>
+			<div className="trafficBody">
+				<div
+					className={selected !== "red" ? "red" : "red selected"}
+					onClick={() => changeColor("red")}></div>
+				<div
+					className={
+						selected !== "yellow" ? "yellow" : "yellow selected"
+					}
+					onClick={() => changeColor("yellow")}></div>
+				<div
+					className={
+						selected !== "green" ? "green" : "green selected"
+					}
+					onClick={() => changeColor("green")}></div>
+			</div>
+		</>
 	);
 }
